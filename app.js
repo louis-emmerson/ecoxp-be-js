0aab70bb0467203a991e8a61a48026cf5f5a1207
emmo2000 <louis.emmerson2000@gmail.com>
2024-11-19 14:53:55 +0000

    get all items

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require("express")
+const { getAllItems } = require("./controllers/items-controller")
 const app = express()
 
-
-
+app.get("/api/items", getAllItems)
 
 module.exports = app
