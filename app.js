9f247e114e82332746498cc658161e489c9bdbca
emmo2000 <louis.emmerson2000@gmail.com>
2024-11-19 15:23:30 +0000

    updated error position for middleware

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express")
 const { getAllItems } = require("./controllers/items-controller")
 const app = express()
 
-app.all("/*", (request, response)=>{
-    response.status(404).send({msg: "Route not found!"})
-})
 
 
 app.get("/api/items", getAllItems)
 
+app.all("/*", (request, response)=>{
+    response.status(404).send({msg: "Route not found!"})
+})
 
 
 module.exports = app
