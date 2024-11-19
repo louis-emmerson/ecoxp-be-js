4007b24c7313d9b2642ebcf73c9ff31a34ca4b96
emmo2000 <louis.emmerson2000@gmail.com>
2024-11-19 15:15:19 +0000

    route not found

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express")
 const { getAllItems } = require("./controllers/items-controller")
 const app = express()
 
+app.all("/*", (request, response)=>{
+    response.status(404).send({msg: "Route not found!"})
+})
+
+
 app.get("/api/items", getAllItems)
+
+
 
 module.exports = app
