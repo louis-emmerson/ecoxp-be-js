ba95ff63abbdaca51614cd5e20fae7418005e18f
Rob Friskney <rfriskney94@gmail.com>
2024-11-21 12:25:37 +0000

    completed add new item functionality

--- controllers/items-controller.js
+++ controllers/items-controller.js
@@ -1,33 +1,52 @@
-const { fetchAllItems, fetchItemById, fetchItemByBarcode } = require("../models/items-models")
+const {
+  fetchAllItems,
+  fetchItemById,
+  fetchItemByBarcode,
+  addNewItem,
+} = require("../models/items-models");
 
-function getAllItems(request, response, next){
-    fetchAllItems()
-    .then((items)=>{
-        response.status(200).send({items})
+function getAllItems(request, response, next) {
+  fetchAllItems()
+    .then((items) => {
+      response.status(200).send({ items });
     })
-    .catch(next)
+    .catch(next);
 }
 
-function getItemById(request, response, next){
+function getItemById(request, response, next) {
+  const { item_id } = request.params;
 
-    const {item_id} =  request.params
-
-    fetchItemById(item_id)
+  fetchItemById(item_id)
     .then((item) => {
-        response.status(200).send({item})
+      response.status(200).send({ item });
     })
-    .catch(next)
+    .catch(next);
 }
 
-function getItemByBarcode(request, response, next){
+function getItemByBarcode(request, response, next) {
+  const { barcode } = request.params;
 
-    const {barcode} = request.params
-
-    fetchItemByBarcode(barcode)
+  fetchItemByBarcode(barcode)
     .then((item) => {
-        response.status(200).send({item})
+      response.status(200).send({ item });
     })
-    .catch(next)
+    .catch(next);
 }
 
-module.exports = {getAllItems, getItemById, getItemByBarcode}
+function postNewItem(request, response, next) {
+  const { material_id, item_name, img_url, barcode } = request.body;
+
+  if (!material_id || !item_name || !img_url || !barcode) {
+    return response.status(400).send({ msg: "Missing required fields" });
+  }
+
+  const newItem = { material_id, item_name, img_url, barcode };
+
+  addNewItem(newItem)
+    .then((item) => {
+      response.status(201).send({ item });
+    })
+    .catch(next);
+}
+
+module.exports = { getAllItems, getItemById, getItemByBarcode, postNewItem };
