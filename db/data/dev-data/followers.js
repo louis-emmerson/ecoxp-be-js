71963699ebd4d2c8b2fe66ea6fce5602478dea16
emmo2000 <louis.emmerson2000@gmail.com>
2024-11-22 14:21:53 +0000

    fixed duplicate import

--- db/data/dev-data/followers.js
+++ db/data/dev-data/followers.js
@@ -26,5 +26,5 @@ module.exports = [
   { "user_id": 3, "follower_id": 4 },
   { "user_id": 4, "follower_id": 5 },
   { "user_id": 5, "follower_id": 1 },
-  { "user_id": 1, "follower_id": 3 }
+  { "user_id": 1, "follower_id": 3 },
 ]
