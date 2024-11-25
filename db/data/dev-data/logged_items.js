e95898a7e4815292e9175aebeae5165204b800dc
Rob Friskney <rfriskney94@gmail.com>
2024-11-25 17:58:15 +0000

    added more data to logged_items (but left existing in)

--- db/data/dev-data/logged_items.js
+++ db/data/dev-data/logged_items.js
@@ -1,5 +1,127 @@
 module.exports = [
   {
+    logged_item_id: 1,
+    item_id: 5,
+    user_id: 1,
+    date: "2024-11-19",
+  },
+  {
+    logged_item_id: 2,
+    item_id: 2,
+    user_id: 2,
+    date: "2024-11-20",
+  },
+  {
+    logged_item_id: 3,
+    item_id: 4,
+    user_id: 2,
+    date: "2024-11-21",
+  },
+  {
+    logged_item_id: 4,
+    item_id: 6,
+    user_id: 3,
+    date: "2024-11-22",
+  },
+  {
+    logged_item_id: 5,
+    item_id: 5,
+    user_id: 5,
+    date: "2024-11-23",
+  },
+  {
+    logged_item_id: 6,
+    item_id: 7,
+    user_id: 3,
+    date: "2024-11-24",
+  },
+  {
+    logged_item_id: 7,
+    item_id: 8,
+    user_id: 4,
+    date: "2024-11-25",
+  },
+  {
+    logged_item_id: 8,
+    item_id: 1,
+    user_id: 2,
+    date: "2024-11-26",
+  },
+  {
+    logged_item_id: 9,
+    item_id: 3,
+    user_id: 1,
+    date: "2024-11-27",
+  },
+  {
+    logged_item_id: 10,
+    item_id: 10,
+    user_id: 5,
+    date: "2024-11-28",
+  },
+  {
+    logged_item_id: 11,
+    item_id: 5,
+    user_id: 4,
+    date: "2024-11-29",
+  },
+
+  // Additional past month entries
+  {
+    logged_item_id: 12,
+    item_id: 2,
+    user_id: 3,
+    date: "2024-10-24",
+  },
+  {
+    logged_item_id: 13,
+    item_id: 11,
+    user_id: 2,
+    date: "2024-10-27",
+  },
+  {
+    logged_item_id: 14,
+    item_id: 4,
+    user_id: 5,
+    date: "2024-10-29",
+  },
+  {
+    logged_item_id: 15,
+    item_id: 9,
+    user_id: 1,
+    date: "2024-10-30",
+  },
+  {
+    logged_item_id: 16,
+    item_id: 13,
+    user_id: 4,
+    date: "2024-10-22",
+  },
+  {
+    logged_item_id: 17,
+    item_id: 12,
+    user_id: 3,
+    date: "2024-10-25",
+  },
+  {
+    logged_item_id: 18,
+    item_id: 6,
+    user_id: 5,
+    date: "2024-10-26",
+  },
+  {
+    logged_item_id: 19,
+    item_id: 8,
+    user_id: 1,
+    date: "2024-10-28",
+  },
+  {
+    logged_item_id: 20,
+    item_id: 3,
+    user_id: 2,
+    date: "2024-10-23",
+  },
+  {
     item_id: 5,
     user_id: 1,
     date: "2024-03-17",
@@ -84,4 +206,4 @@ module.exports = [
     user_id: 2,
     date: "2024-11-19",
   },
-]
+];
